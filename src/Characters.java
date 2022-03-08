9b406bd8f27bc0ea653772131be5e81d858bba90
bjorn <brunocarrarabpc@gmail.com>
2022-03-08 00:05:59 -0300

    [11] First commit

---
+++ src/Characters.java
@@ -0,0 +1,104 @@
+public class Characters {
+    private int health;
+    private int sanity;
+    private int agility;
+    private int strength;
+    private int vigor;
+    private int presence;
+    private int intelect;
+    private int exp;
+    private String status;
+    private String name;
+    private String author;
+
+    public Characters() {
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSanity() {
+        return sanity;
+    }
+
+    public void setSanity(int sanity) {
+        this.sanity = sanity;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public void setAgility(int agility) {
+        this.agility = agility;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getVigor() {
+        return vigor;
+    }
+
+    public void setVigor(int vigor) {
+        this.vigor = vigor;
+    }
+
+    public int getPresence() {
+        return presence;
+    }
+
+    public void setPresence(int presence) {
+        this.presence = presence;
+    }
+
+    public int getIntelect() {
+        return intelect;
+    }
+
+    public void setIntelect(int intelect) {
+        this.intelect = intelect;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
